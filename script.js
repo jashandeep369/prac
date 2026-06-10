c1fe3321bed5e1ac38d2576c3abeaa9ab8cd09cf
jashandeep369 <jdeep.singh2008@gmail.com>
2026-06-10 12:02:58 +0530

    Update script.js
    updated

--- script.js
+++ script.js
@@ -65,3 +65,88 @@ const correct_answer = 8
 const message = player_guess === correct_answer ? "you won the game" : player_guess < correct_answer ? "too low" : "too high"
 
 console.log(message)
+
+
+function select_item(item) {
+  let price = 0
+
+  switch(item) {
+    case "Coffee":
+      price = 2
+      break
+    case "Sandwich":
+      price = 5
+      break
+    case "Salad":
+      price = 4
+      break
+    case "Lemon Cake":
+      price = 3
+      break
+    default:
+      console.log("we dont have that")
+  }
+  return `you selected ${item} that will be $${price}`
+}
+
+console.log(select_item("biscuit"))
+
+const dream_holiday = {
+  destination : "chile",
+  activity : "hiking",
+  companion : "lifepartner",
+
+
+
+}
+
+const {destination , activity , companion} = dream_holiday
+
+console.log(`my dream destination is ${destination}`)
+
+
+
+function log_answer(answer,points) {
+  console.log(`the answer of the question is ${answer} and if u got it get ${points} points`)
+}
+
+game = setTimeout(log_answer,3000,"lima" , 10)
+
+clearTimeout(game)
+console.log("canceling ..........")
+
+time = new Date()
+console.log((time.getFullYear()).toString())
+
+function greeting(username) {
+  if (username) {
+    console.log(`hello ${username}`)
+
+  }
+  else {
+    throw new Error("no username provided")
+  }
+}
+
+//greeting()
+
+
+// String()
+//   Number()
+//Boolean()
+//Array()
+//Object()
+
+current_ticket_number = 0
+
+function ticket_to_customer() {
+  return  ++current_ticket_number
+}
+
+console.log(`the ticket number of 1st customerc. is: ${ticket_to_customer()}`)
+console.log(`the ticket number of 2st customerc. is: ${ticket_to_customer()}`)
+console.log(`the ticket number of 3st customerc. is: ${ticket_to_customer()}`)
+
+number = 332_34343_423_23423_4242_423n
+
+console.log(typeof number)
