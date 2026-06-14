7e4facac3c6280181cecab499ad33669e9f8c015
jashandeep369 <jdeep.singh2008@gmail.com>
2026-06-14 11:42:10 +0530

    shopping list

--- script.js
+++ script.js
@@ -418,3 +418,65 @@ console.log(sentence.replaceAll('i', 'I'))
 
 
 
+/*
+Challenge:
+    1. Convert this function declaration to a function expression.
+*/
+
+getTheftAlert = function (numberOfTransactionsHour) {
+    if (numberOfTransactionsHour > 5) {
+        return `You have made ${numberOfTransactionsHour} transactions 
+                in the past hour. We think your card might have been 
+                compromised`
+    }
+}
+
+console.log(getTheftAlert(6))
+
+import { itemsBoughtArr } from '/itemsBoughtArr.js'
+
+function calculateTotalCost(itemsBoughtArr){
+/*
+Challenge:
+1. Use the reduce method to calculate the total 
+   cost of items which have been bought.
+*/ 
+    const total = itemsBoughtArr.reduce((total, currentItem) => total + currentItem.priceUSD, 0
+        )
+    return total
+}
+
+console.log(calculateTotalCost(itemsBoughtArr))
+
+import { itemsBoughtArr } from '/itemsBoughtArr.js'
+
+function calculateTotalCost(itemsBoughtArr, discount = 10){
+    const total = itemsBoughtArr.reduce((total, currentItem)=>
+        total + currentItem.priceUSD, 0
+        )
+    return total - discount
+}
+
+
+console.log(calculateTotalCost(itemsBoughtArr, 20))
+
+import { itemsBoughtArr } from '/itemsBoughtArr.js'
+
+function calculateTotalCost(itemsBoughtArr, discount = 10){
+    const total = itemsBoughtArr.reduce((total, currentItem)=>
+        total + currentItem.priceUSD, 0
+        )
+    return total - discount
+}
+
+console.log(calculateTotalCost(itemsBoughtArr, 20))
+
+function setPermissionLevel(permissionLevel, ...names) {
+    
+    names.forEach((name)=> 
+        console.log(`${name} now has ${permissionLevel} level access.`))
+    
+}
+
+setPermissionLevel('admin', 'Dave', 'Sally')
+ 
